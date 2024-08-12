8503f31b1d114fe162a24d4a38cc84f4b296be84
Nathan Cox <nate@campswim.co>
2024-08-12 10:23:35 -0700

    Remove the 'old' directory.

--- old/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = props => (
-  <footer className='footer'>
-    <p>&copy; nCompass | <a href="mailto:chad@ncompass.org">Email Us</a></p>
-  </footer>
-);
-
-export default Footer;
